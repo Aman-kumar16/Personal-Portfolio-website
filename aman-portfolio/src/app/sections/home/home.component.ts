39b2d9778ff974464295e808a9983fead58eaf47
Aman kumar <kumar.ak16121999@gmail.com>
2025-04-05 20:01:14 +0200

    add background image for homepage

--- aman-portfolio/src/app/sections/home/home.component.ts
+++ aman-portfolio/src/app/sections/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,39 +7,14 @@ import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.createCodeRain();
     this.animateText();
   }
 
-  createCodeRain(): void {
-    const backgroundAnimation = this.el.nativeElement.querySelector(
-      '.background-animation'
-    );
-    const numberOfCharacters = window.innerWidth / 5;
-
-    for (let i = 0; i < numberOfCharacters; i++) {
-      const span = this.renderer.createElement('span');
-      span.textContent = this.getRandomCharacter();
-      const initialX = Math.random() * window.innerWidth;
-      const initialDelay = Math.random() * 3;
-
-      this.renderer.setStyle(span, 'left', `${initialX}px`);
-      this.renderer.setStyle(span, 'animation-delay', `-${initialDelay}s`);
-      this.renderer.appendChild(backgroundAnimation, span);
-    }
-  }
-
-  getRandomCharacter(): string {
-    const characters = '01$#%&@01';
-    return characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-
   animateText(): void {
-    const animatedElements =
-      this.el.nativeElement.querySelectorAll('.animated-text');
+    const animatedElements = document.querySelectorAll('.animated-text') as NodeListOf<HTMLElement>;
     animatedElements.forEach((element: HTMLElement) => {
       const text = element.textContent;
       if (text) {
