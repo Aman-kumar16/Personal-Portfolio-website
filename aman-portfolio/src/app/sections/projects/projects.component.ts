8f92931b87735f0d2f6ebcae6793ed0b64d522ee
Aman kumar <kumar.ak16121999@gmail.com>
2025-04-19 15:19:56 +0200

    adding project components page

--- aman-portfolio/src/app/sections/projects/projects.component.ts
+++ aman-portfolio/src/app/sections/projects/projects.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      name: 'Personal Portfolio Website',
+      description: 'A portfolio website built with HTML, CSS, and Typescript to showcase my work, skills, and resume.',
+      url: 'https://github.com/Aman-kumar16/Personal-Portfolio-website',
+      tech: ['HTML', 'CSS', 'Typescipt']
+    },
+    {
+      name: 'Currency Conversion Microservice',
+      description: 'A Java application that performs real-time currency conversion by integrating with an external exchange rate API. It demonstrates effective use of REST API consumption and JSON parsing to calculate accurate conversions between various currencies.',
+      url: 'https://github.com/Aman-kumar16/Currency-Conversion-Project',
+      tech: ['Java', 'REST APIs', 'JSON', 'HTTP Client']
+    }
+  ];
 }
